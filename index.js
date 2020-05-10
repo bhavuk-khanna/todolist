f47f91df9684adaf3d21631bf8128430acf2fcb0
bhavuk-khanna <bhavukkhanna@gmail.com>
2020-05-10 22:36:31 +0530

    created front end view

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 const port = 8000;
 
 
+//use express router 
+
+app.use("/",require('./routes'));
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views', './views');
+
+
+//set up folder for static files
+app.use(express.static('assets'));
 
 
 app.listen(port,function(err){
